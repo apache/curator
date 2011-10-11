19c79f38cccdcaddd9b0b065264bf73f8cbc89d4
Jordan Zimmerman <jordan@jordanzimmerman.com>
2011-10-11 11:25:30 -0700

    Esure that testSimultaneous really is simultaneous

--- curator-client/src/test/java/com/netflix/curator/TestEnsurePath.java
+++ curator-client/src/test/java/com/netflix/curator/TestEnsurePath.java
@@ -95,6 +95,7 @@ public class TestEnsurePath
                     public Void call() throws Exception
                     {
                         startedLatch.countDown();
+                        Assert.assertTrue(startedLatch.await(10, TimeUnit.SECONDS));
                         ensurePath.ensure(curator);
                         finishedLatch.countDown();
                         return null;
