10f054961b37ed17cf1ee679a5b988a302c7c1f4
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-03-23 14:16:09 -0700

    Will be used for various debug stuff

---
+++ curator-client/src/main/java/com/netflix/curator/utils/DebugUtils.java
@@ -0,0 +1,10 @@
+package com.netflix.curator.utils;
+
+public class DebugUtils
+{
+    public static final String          PROPERTY_LOG_EVENTS = "curator-log-events";
+
+    private DebugUtils()
+    {
+    }
+}
