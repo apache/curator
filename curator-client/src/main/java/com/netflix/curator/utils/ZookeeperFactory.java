f9ca3cf1b6ec3cdb5a06b1f40ebd7a6dc451a36a
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-04-12 21:39:54 -0700

    Javadoc

--- curator-client/src/main/java/com/netflix/curator/utils/ZookeeperFactory.java
+++ curator-client/src/main/java/com/netflix/curator/utils/ZookeeperFactory.java
@@ -5,5 +5,14 @@ import org.apache.zookeeper.ZooKeeper;
 
 public interface ZookeeperFactory
 {
+    /**
+     * Allocate a new ZooKeeper instance
+     *
+     * @param connectString the connection string
+     * @param sessionTimeout session timeout in milliseconds
+     * @param watcher optional watcher
+     * @return the instance
+     * @throws Exception errors
+     */
     public ZooKeeper        newZooKeeper(String connectString, int sessionTimeout, Watcher watcher) throws Exception;
 }
