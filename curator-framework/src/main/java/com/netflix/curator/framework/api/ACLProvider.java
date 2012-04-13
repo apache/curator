7737366a807c599c40d8ac40fabccc8e20c2515f
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-04-13 14:07:00 -0700

    Javadoc

--- curator-framework/src/main/java/com/netflix/curator/framework/api/ACLProvider.java
+++ curator-framework/src/main/java/com/netflix/curator/framework/api/ACLProvider.java
@@ -1,11 +1,23 @@
 package com.netflix.curator.framework.api;
 
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.data.ACL;
 import java.util.List;
 
 public interface ACLProvider
 {
+    /**
+     * Return the ACL list to use by default (usually {@link ZooDefs.Ids#OPEN_ACL_UNSAFE}).
+     *
+     * @return default ACL list
+     */
     public List<ACL>        getDefaultAcl();
 
+    /**
+     * Return the ACL list to use for the given path
+     *
+     * @param path path (NOTE: might be null)
+     * @return ACL list
+     */
     public List<ACL>        getAclForPath(String path);
 }
