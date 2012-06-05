90c7e7a7c4ee6f85317c190d65097935f4568860
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-06-05 13:21:45 -0700

    Fixed test name

---
+++ curator-recipes/src/test/java/com/netflix/curator/framework/recipes/locks/TestLockCleanlinessWithFaults.java
@@ -0,0 +1,55 @@
+package com.netflix.curator.framework.recipes.locks;
+
+import com.google.common.io.Closeables;
+import com.netflix.curator.framework.CuratorFramework;
+import com.netflix.curator.framework.CuratorFrameworkFactory;
+import com.netflix.curator.framework.recipes.BaseClassForTests;
+import com.netflix.curator.retry.RetryNTimes;
+import junit.framework.Assert;
+import org.apache.zookeeper.KeeperException;
+import org.testng.annotations.Test;
+import java.util.List;
+
+public class TestLockCleanlinessWithFaults extends BaseClassForTests
+{
+    @Test
+    public void     testNodeDeleted() throws Exception
+    {
+        final String PATH = "/foo/bar";
+
+        CuratorFramework        client = null;
+        try
+        {
+            client = CuratorFrameworkFactory.newClient(server.getConnectString(), new RetryNTimes(0, 0));
+            client.start();
+
+            client.create().creatingParentsIfNeeded().forPath(PATH);
+            Assert.assertEquals(client.checkExists().forPath(PATH).getNumChildren(), 0);
+
+            LockInternals       internals = new LockInternals(client, new StandardLockInternalsDriver(), PATH, "lock-", 1)
+            {
+                @Override
+                List<String> getSortedChildren() throws Exception
+                {
+                    throw new KeeperException.NoNodeException();
+                }
+            };
+            try
+            {
+                internals.attemptLock(0, null, null);
+                Assert.fail();
+            }
+            catch ( KeeperException.NoNodeException dummy )
+            {
+                // expected
+            }
+
+            // make sure no nodes are left lying around
+            Assert.assertEquals(client.checkExists().forPath(PATH).getNumChildren(), 0);
+        }
+        finally
+        {
+            Closeables.closeQuietly(client);
+        }
+    }
+}
