8cae5ef9fbf170a07909d56c9aa08d24338e565a
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-06-21 23:44:19 -0700

    Added a new utility class: Reaper.java. Curator users can allocate a reaper and have it periodically check for empty lock nodes and delete them.

---
+++ curator-recipes/src/main/java/com/netflix/curator/framework/recipes/locks/Reaper.java
@@ -0,0 +1,224 @@
+package com.netflix.curator.framework.recipes.locks;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.netflix.curator.framework.CuratorFramework;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A Utility to delete parent paths of locks, etc. Periodically checks paths added to the reaper.
+ * If at check time, there are no children, the path is deleted.
+ */
+public class Reaper implements Closeable
+{
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final CuratorFramework client;
+    private final ExecutorService executor;
+    private final int reapingThresholdMs;
+    private final DelayQueue<PathHolder> queue = new DelayQueue<PathHolder>();
+
+    private volatile Future<Void> task;
+
+    private static final int        REAPING_THRESHOLD_MS = (int)TimeUnit.MILLISECONDS.convert(5, TimeUnit.MINUTES);
+
+    private static class PathHolder implements Delayed
+    {
+        private final String path;
+        private final long expirationMs;
+
+        private PathHolder(String path, int delayMs)
+        {
+            this.path = path;
+            this.expirationMs = System.currentTimeMillis() + delayMs;
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit)
+        {
+            return unit.convert(expirationMs - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        }
+
+        @Override
+        public int compareTo(Delayed o)
+        {
+            long        diff = getDelay(TimeUnit.MILLISECONDS) - o.getDelay(TimeUnit.MILLISECONDS);
+            return (diff < 0) ? -1 : ((diff > 0) ? 1 : 0);
+        }
+
+        @SuppressWarnings("RedundantIfStatement")
+        @Override
+        public boolean equals(Object o)
+        {
+            if ( this == o )
+            {
+                return true;
+            }
+            if ( o == null || getClass() != o.getClass() )
+            {
+                return false;
+            }
+
+            PathHolder that = (PathHolder)o;
+
+            if ( path != null ? !path.equals(that.path) : that.path != null )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return path.hashCode();
+        }
+    }
+
+    /**
+     * Uses the default reaping threshold of 5 minutes and creates an internal thread pool
+     *
+     * @param client client
+     */
+    public Reaper(CuratorFramework client)
+    {
+        this(client, Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setNameFormat("Reaper-%d").build()), REAPING_THRESHOLD_MS);
+    }
+
+    /**
+     * Uses the given reaping threshold and creates an internal thread pool
+     *
+     * @param client client
+     * @param reapingThresholdMs threshold in milliseconds that determines that a path can be deleted
+     */
+    public Reaper(CuratorFramework client, int reapingThresholdMs)
+    {
+        this(client, Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setNameFormat("Reaper-%d").build()), reapingThresholdMs);
+    }
+
+    /**
+     * @param client client
+     * @param executor thread pool
+     * @param reapingThresholdMs threshold in milliseconds that determines that a path can be deleted
+     */
+    public Reaper(CuratorFramework client, ExecutorService executor, int reapingThresholdMs)
+    {
+        this.client = client;
+        this.executor = executor;
+        this.reapingThresholdMs = reapingThresholdMs;
+    }
+
+    /**
+     * Add a path to be checked by the reaper. The path will be checked periodically
+     * until the path is removed of the reaper is closed.
+     *
+     * @param path path to check
+     */
+    public void     addPath(String path)
+    {
+        queue.add(new PathHolder(path, reapingThresholdMs));
+    }
+
+    /**
+     * Stop reaping the given path
+     *
+     * @param path path to remove
+     * @return true if the path was removed
+     */
+    public boolean     removePath(String path)
+    {
+        return queue.remove(new PathHolder(path, reapingThresholdMs));
+    }
+
+    /**
+     * The reaper must be started
+     *
+     * @throws Exception errors
+     */
+    public void start() throws Exception
+    {
+        task = executor.submit
+        (
+            new Callable<Void>()
+            {
+                @Override
+                public Void call() throws Exception
+                {
+                    try
+                    {
+                        while ( !Thread.currentThread().isInterrupted() )
+                        {
+                            reap(queue.take().path);
+                        }
+                    }
+                    catch ( InterruptedException e )
+                    {
+                        Thread.currentThread().interrupt();
+                    }
+                    return null;
+                }
+            }
+        );
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        try
+        {
+            queue.clear();
+            task.cancel(true);
+        }
+        catch ( Exception e )
+        {
+            log.error("Canceling task", e);
+        }
+    }
+
+    private void reap(String path)
+    {
+        try
+        {
+            Stat        stat = client.checkExists().forPath(path);
+            if ( stat != null ) // otherwise already deleted
+            {
+                if ( stat.getNumChildren() == 0 )
+                {
+                    try
+                    {
+                        client.delete().forPath(path);
+                        log.info("Reaping path: " + path);
+                    }
+                    catch ( KeeperException.NoNodeException ignore )
+                    {
+                        // ignore - it must have been deleted by another process/thread
+                    }
+                    catch ( KeeperException.NotEmptyException ignore )
+                    {
+                        // ignore - it must have been re-used
+                    }
+                }
+            }
+        }
+        catch ( Exception e )
+        {
+            log.error("Trying to reap: " + path, e);
+        }
+
+        if ( !Thread.currentThread().isInterrupted() )
+        {
+            addPath(path);
+        }
+    }
+}
