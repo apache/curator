01719d4c844a6bb843bf2ea4071b5551323c330e
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-07-23 16:53:45 -0700

    IDEA killed many of my class docs - adding them back

--- curator-recipes/src/main/java/com/netflix/curator/framework/recipes/locks/InterProcessReadWriteLock.java
+++ curator-recipes/src/main/java/com/netflix/curator/framework/recipes/locks/InterProcessReadWriteLock.java
@@ -23,6 +23,32 @@ import com.netflix.curator.framework.CuratorFramework;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * <p>
+ *    A re-entrant read/write mutex that works across JVMs. Uses Zookeeper to hold the lock. All processes
+ *    in all JVMs that use the same lock path will achieve an inter-process critical section. Further, this mutex is
+ *    "fair" - each user will get the mutex in the order requested (from ZK's point of view).
+ * </p>
+ *
+ * <p>
+ *    A read write lock maintains a pair of associated locks, one for read-only operations and one
+ *    for writing. The read lock may be held simultaneously by multiple reader processes, so long as
+ *    there are no writers. The write lock is exclusive.
+ * </p>
+ *
+ * <p>
+ *    <b>Reentrancy</b><br/>
+ *    This lock allows both readers and writers to reacquire read or write locks in the style of a
+ *    re-entrant lock. Non-re-entrant readers are not allowed until all write locks held by the
+ *    writing thread/process have been released. Additionally, a writer can acquire the read lock, but not
+ *    vice-versa. If a reader tries to acquire the write lock it will never succeed.<br/><br/>
+ *
+ *    <b>Lock downgrading</b><br/>
+ *    Re-entrancy also allows downgrading from the write lock to a read lock, by acquiring the write
+ *    lock, then the read lock and then releasing the write lock. However, upgrading from a read
+ *    lock to the write lock is not possible.
+ * </p>
+ */
 public class InterProcessReadWriteLock
 {
     private final InterProcessMutex readMutex;
