05d5420f10d72df52a355354d027639170044b99
randgalt <randgalt@apache.org>
2014-03-07 08:25:35 -0500

    more read/write tests

--- curator-recipes/src/test/java/org/apache/curator/framework/recipes/locks/TestInterProcessReadWriteLockBase.java
+++ curator-recipes/src/test/java/org/apache/curator/framework/recipes/locks/TestInterProcessReadWriteLockBase.java
@@ -19,6 +19,8 @@
 
 package org.apache.curator.framework.recipes.locks;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.BaseClassForTests;
@@ -27,17 +29,130 @@ import org.apache.curator.test.Timing;
 import org.apache.curator.utils.CloseableUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.util.Collection;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class TestInterProcessReadWriteLockBase extends BaseClassForTests
 {
+    @Test
+    public void testOrdering() throws Exception
+    {
+        Timing timing = new Timing();
+
+        ExecutorService service = Executors.newCachedThreadPool();
+        try
+        {
+            Semaphore advanceSemaphore = new Semaphore(0);
+            Semaphore acquiredSemaphore = new Semaphore(0);
+            Semaphore stagingSemaphore = new Semaphore(0);
+            Set<Integer> lockers = Sets.newSetFromMap(Maps.<Integer, Boolean>newConcurrentMap());
+            for ( int i = 1; i <= 3; ++i )
+            {
+                service.submit(orderingProc(timing, null, advanceSemaphore, acquiredSemaphore, lockers, i, false));
+            }
+
+            Assert.assertTrue(timing.acquireSemaphore(acquiredSemaphore, 3));
+            service.submit(orderingProc(timing, null, advanceSemaphore, acquiredSemaphore, lockers, 4, true));
+            timing.sleepABit();
+
+            // writer should not succeed yet
+            Assert.assertEquals(lockers, Sets.newHashSet(1, 2, 3));
+            lockers.clear();
+            advanceSemaphore.release(3);
+
+            Assert.assertTrue(timing.acquireSemaphore(acquiredSemaphore, 1));
+            service.submit(orderingProc(timing, stagingSemaphore, advanceSemaphore, acquiredSemaphore, lockers, 5, false));
+            Assert.assertTrue(timing.acquireSemaphore(stagingSemaphore));
+            timing.sleepABit();
+            service.submit(orderingProc(timing, stagingSemaphore, advanceSemaphore, acquiredSemaphore, lockers, 6, false));
+            Assert.assertTrue(timing.acquireSemaphore(stagingSemaphore));
+            timing.sleepABit();
+            service.submit(orderingProc(timing, stagingSemaphore, advanceSemaphore, acquiredSemaphore, lockers, 7, true));
+            Assert.assertTrue(timing.acquireSemaphore(stagingSemaphore));
+            timing.sleepABit();
+            service.submit(orderingProc(timing, stagingSemaphore, advanceSemaphore, acquiredSemaphore, lockers, 8, false));
+            Assert.assertTrue(timing.acquireSemaphore(stagingSemaphore));
+            timing.sleepABit();
+
+            // new readers/writers should not succeed yet
+            Assert.assertEquals(lockers, Sets.newHashSet(4));
+            lockers.clear();
+            advanceSemaphore.release(1);
+
+            Assert.assertTrue(timing.acquireSemaphore(acquiredSemaphore, 2));
+            timing.sleepABit();
+
+            // only 2 readers should succeed next
+            Assert.assertEquals(lockers, Sets.newHashSet(5, 6));
+            lockers.clear();
+            advanceSemaphore.release(2);
+            timing.sleepABit();
+
+            // now 1 writer
+            Assert.assertEquals(lockers, Sets.newHashSet(7));
+            lockers.clear();
+            advanceSemaphore.release(1);
+            timing.sleepABit();
+
+            // last reader
+            Assert.assertEquals(lockers, Sets.newHashSet(8));
+            advanceSemaphore.release(1);
+        }
+        finally
+        {
+            service.shutdownNow();
+        }
+    }
+
+    protected Callable<Void> orderingProc(final Timing timing, final Semaphore stagingSemaphore, final Semaphore advanceSemaphore, final Semaphore acquiredSemaphore, final Collection<Integer> lockers, final int index, final boolean isWriter)
+    {
+        return new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                CuratorFramework client = CuratorFrameworkFactory.newClient(server.getConnectString(), new RetryOneTime(1));
+                try
+                {
+                    client.start();
+                    InterProcessReadWriteLockBase lock = newLock(client, "/lock");
+                    InterProcessLock thisLock = isWriter ? lock.writeLock() : lock.readLock();
+
+                    if ( stagingSemaphore != null )
+                    {
+                        stagingSemaphore.release();
+                    }
+
+                    Assert.assertTrue(thisLock.acquire(timing.forWaiting().seconds(), TimeUnit.SECONDS));
+                    try
+                    {
+                        acquiredSemaphore.release();
+                        lockers.add(index);
+                        Assert.assertTrue(timing.acquireSemaphore(advanceSemaphore));
+                    }
+                    finally
+                    {
+                        thisLock.release();
+                    }
+                }
+                finally
+                {
+                    CloseableUtils.closeQuietly(client);
+                }
+                return null;
+            }
+        };
+    }
+
     @Test
     public void testWriterAgainstConstantReaders() throws Exception
     {
