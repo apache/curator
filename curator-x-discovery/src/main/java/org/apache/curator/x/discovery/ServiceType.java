877eb55c93dc284339a86fdaf678ceba0674d808
David Trott <github@davidtrott.com>
2014-03-08 12:29:50 -0800

    Documented ServiceType's.

--- curator-x-discovery/src/main/java/org/apache/curator/x/discovery/ServiceType.java
+++ curator-x-discovery/src/main/java/org/apache/curator/x/discovery/ServiceType.java
@@ -18,9 +18,39 @@
  */
 package org.apache.curator.x.discovery;
 
+/**
+ * The type of the service registration.
+ *
+ * STATIC and PERMANENT registrations are created when the x-discovery-server rest service registers another service.
+ * Where as DYNAMIC registrations are typically used for everything else
+ * (including the definition of the x-discovery-server rest service itself).
+ */
 public enum ServiceType
 {
+    /**
+     * DYNAMIC registrations (default) are tied to the lifecycle of the creating process.
+     *
+     * ServiceDiscoveryImpl.internalRegisterService() maps DYNAMIC registrations to EPHEMERAL nodes as a result they
+     * don't need to be manually cleaned up.
+     */
     DYNAMIC,
+
+    /**
+     * STATIC registrations require the caller to regularly re-register the service before a timeout expires.
+     *
+     * The timeout is not defined in the curator code (the caller must define the timeout).
+     *
+     * However InstanceCleanup.checkService implements the timeout process to expire them after the timeout.
+     * Even though STATIC registrations are mapped to PERSISTENT zookeeper nodes, they are also cleaned up
+     * during a clean shutdown of the x-discovery-server rest service ServiceDiscoveryImpl.close().
+     */
     STATIC,
+
+    /**
+     * PERMANENT registrations are not tied to the existence of any particular process.
+     *
+     * They can be (un)registered through the x-discovery-server rest service, however the process immediately discards
+     * all knowledge of the registration ServiceDiscoveryImpl.registerService(...)
+     */
     PERMANENT
 }
