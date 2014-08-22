d589063ecd4d64f07244e9e5264b1e36976faa50
Scott Blum <scottb@squareup.com>
2014-08-22 16:34:29 -0400

    CURATOR-145: TreeCache should implement maxDepth

--- curator-recipes/src/test/java/org/apache/curator/framework/recipes/cache/TestTreeCacheRandomTree.java
+++ curator-recipes/src/test/java/org/apache/curator/framework/recipes/cache/TestTreeCacheRandomTree.java
@@ -50,20 +50,39 @@ public class TestTreeCacheRandomTree extends BaseTestTreeCache
     // These constants will produce a tree about 10 levels deep.
     private static final int ITERATIONS = 1000;
     private static final double DIVE_CHANCE = 0.9;
+    private static final int TEST_DEPTH = 5;
 
     private final Random random = new Random();
+    private boolean withDepth = false;
+
+    @Test
+    public void testGiantRandomDeepTree() throws Exception {
+        doTestGiantRandomDeepTree();
+    }
+
+    @Test
+    public void testGiantRandomDeepTreeWithDepth() throws Exception {
+        withDepth = true;
+        doTestGiantRandomDeepTree();
+    }
 
     /**
      * Randomly construct a large tree of test data in memory, mirror it into ZK, and then use
      * a TreeCache to follow the changes.  At each step, assert that TreeCache matches our
      * source-of-truth test data, and that we see exactly the set of events we expect to see.
      */
-    @Test
-    public void testGiantRandomDeepTree() throws Exception
+    private void doTestGiantRandomDeepTree() throws Exception
     {
         client.create().forPath("/tree", null);
         CuratorFramework cl = client.usingNamespace("tree");
-        cache = newTreeCacheWithListeners(cl, "/");
+        if ( withDepth )
+        {
+            cache = buildWithListeners(TreeCache.newBuilder(cl, "/").setMaxDepth(TEST_DEPTH));
+        }
+        else
+        {
+            cache = newTreeCacheWithListeners(cl, "/");
+        }
         cache.start();
         assertEvent(TreeCacheEvent.Type.NODE_ADDED, "/");
         assertEvent(TreeCacheEvent.Type.INITIALIZED);
@@ -104,7 +123,10 @@ public class TestTreeCacheRandomTree extends BaseTestTreeCache
                     cl.delete().forPath(node.fullPath);
 
                     // TreeCache should see the delete.
-                    assertEvent(TreeCacheEvent.Type.NODE_REMOVED, node.fullPath);
+                    if (shouldSeeEventAt(node.fullPath))
+                    {
+                        assertEvent(TreeCacheEvent.Type.NODE_REMOVED, node.fullPath);
+                    }
                     ++removals;
                 }
                 break;
@@ -126,7 +148,10 @@ public class TestTreeCacheRandomTree extends BaseTestTreeCache
                 cl.setData().forPath(node.fullPath, node.data);
 
                 // TreeCache should see the update.
-                assertEvent(TreeCacheEvent.Type.NODE_UPDATED, node.fullPath, node.data);
+                if (shouldSeeEventAt(node.fullPath))
+                {
+                    assertEvent(TreeCacheEvent.Type.NODE_UPDATED, node.fullPath, node.data);
+                }
 
                 ++updates;
                 break;
@@ -149,7 +174,10 @@ public class TestTreeCacheRandomTree extends BaseTestTreeCache
                 cl.create().forPath(child.fullPath, child.data);
 
                 // TreeCache should see the add.
-                assertEvent(TreeCacheEvent.Type.NODE_ADDED, child.fullPath, child.data);
+                if (shouldSeeEventAt(child.fullPath))
+                {
+                    assertEvent(TreeCacheEvent.Type.NODE_ADDED, child.fullPath, child.data);
+                }
 
                 ++adds;
                 break;
@@ -157,7 +185,7 @@ public class TestTreeCacheRandomTree extends BaseTestTreeCache
 
             // Each iteration, ensure the cached state matches our source-of-truth tree.
             assertNodeEquals(cache.getCurrentData("/"), root);
-            assertTreeEquals(cache, root);
+            assertTreeEquals(cache, root, 0);
         }
 
         // Typical stats for this test: maxDepth: 10, adds: 349, removals: 198, updates: 320
@@ -167,13 +195,26 @@ public class TestTreeCacheRandomTree extends BaseTestTreeCache
     }
 
     /**
+     * Returns true we should see an event at this path based on maxDepth, false otherwise.
+     */
+    private boolean shouldSeeEventAt(String fullPath)
+    {
+        return !withDepth || ZKPaths.split(fullPath).size() <= TEST_DEPTH;
+    }
+
+    /**
      * Recursively assert that current children equal expected children.
      */
-    private static void assertTreeEquals(TreeCache cache, TestNode expectedNode)
+    private void assertTreeEquals(TreeCache cache, TestNode expectedNode, int depth)
     {
         String path = expectedNode.fullPath;
         Map<String, ChildData> cacheChildren = cache.getCurrentChildren(path);
         Assert.assertNotNull(cacheChildren, path);
+
+        if (withDepth && depth == TEST_DEPTH) {
+            return;
+        }
+
         Assert.assertEquals(cacheChildren.keySet(), expectedNode.children.keySet(), path);
 
         for ( Map.Entry<String, TestNode> entry : expectedNode.children.entrySet() )
@@ -182,7 +223,7 @@ public class TestTreeCacheRandomTree extends BaseTestTreeCache
             ChildData childData = cacheChildren.get(nodeName);
             TestNode expectedChild = entry.getValue();
             assertNodeEquals(childData, expectedChild);
-            assertTreeEquals(cache, expectedChild);
+            assertTreeEquals(cache, expectedChild, depth + 1);
         }
     }
 
