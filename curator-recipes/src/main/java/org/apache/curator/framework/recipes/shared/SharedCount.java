fc4b905f6e98074812f3bed891c3e79d0b7503c3
randgalt <randgalt@apache.org>
2017-07-11 08:56:51 -0500

    Merge branch 'patch-1' of github.com:nivs/curator into CURATOR-420

--- curator-recipes/src/main/java/org/apache/curator/framework/recipes/shared/SharedCount.java
+++ curator-recipes/src/main/java/org/apache/curator/framework/recipes/shared/SharedCount.java
@@ -146,7 +146,10 @@ public class SharedCount implements Closeable, SharedCountReader, Listenable<Sha
     @Override
     public void     removeListener(SharedCountListener listener)
     {
-        listeners.remove(listener);
+        SharedValueListener valueListener = listeners.remove(listener);
+        if(valueListener != null) {
+            sharedValue.getListenable().removeListener(valueListener);
+        }
     }
 
     /**
