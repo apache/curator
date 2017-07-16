c6f7aeb39957158fad0207614583f867729e7770
randgalt <randgalt@apache.org>
2017-07-16 14:44:56 -0500

    GetDataBuilderImpl() wasn't handling storingStatIn for async because the old DSL didn't support it

--- curator-x-async/src/test/java/org/apache/curator/x/async/TestBasicOperations.java
+++ curator-x-async/src/test/java/org/apache/curator/x/async/TestBasicOperations.java
@@ -189,4 +189,14 @@ public class TestBasicOperations extends CompletableBaseClassForTests
             Assert.assertEquals(v.getCode(), KeeperException.Code.CONNECTIONLOSS);
         });
     }
+
+    @Test
+    public void testGetDataWithStat()
+    {
+        complete(client.create().forPath("/test", "hey".getBytes()));
+
+        Stat stat = new Stat();
+        complete(client.getData().storingStatIn(stat).forPath("/test"));
+        Assert.assertEquals(stat.getDataLength(), "hey".length());
+    }
 }
