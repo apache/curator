097083c4a2cd2e252c6f3b57e3e837e4a9ecabc9
randgalt <randgalt@apache.org>
2017-07-20 10:53:52 -0500

    TestTreeCache now uses the improved KillSession2 but that results in different event ordering

--- curator-recipes/src/test/java/org/apache/curator/framework/recipes/cache/TestTreeCache.java
+++ curator-recipes/src/test/java/org/apache/curator/framework/recipes/cache/TestTreeCache.java
@@ -424,11 +424,10 @@ public class TestTreeCache extends BaseTestTreeCache
         assertEvent(TreeCacheEvent.Type.NODE_ADDED, "/test/me");
 
         KillSession2.kill(client.getZookeeperClient().getZooKeeper());
-        assertEvent(TreeCacheEvent.Type.CONNECTION_SUSPENDED);
         assertEvent(TreeCacheEvent.Type.CONNECTION_LOST);
         assertEvent(TreeCacheEvent.Type.CONNECTION_RECONNECTED);
-        assertEvent(TreeCacheEvent.Type.NODE_REMOVED, "/test/me", "data".getBytes());
         assertEvent(TreeCacheEvent.Type.INITIALIZED);
+        assertEvent(TreeCacheEvent.Type.NODE_REMOVED, "/test/me", "data".getBytes());
 
         assertNoMoreEvents();
     }
