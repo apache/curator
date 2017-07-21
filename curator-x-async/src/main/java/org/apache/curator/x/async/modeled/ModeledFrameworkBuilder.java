ee6859eeed4ed13b018de409726052d47f3344bc
randgalt <randgalt@apache.org>
2017-07-21 04:17:22 -0500

    Oops - default ctor ModeledFrameworkBuilder didn't set modeledOptions

--- curator-x-async/src/main/java/org/apache/curator/x/async/modeled/ModeledFrameworkBuilder.java
+++ curator-x-async/src/main/java/org/apache/curator/x/async/modeled/ModeledFrameworkBuilder.java
@@ -161,6 +161,7 @@ public class ModeledFrameworkBuilder<T>
 
     ModeledFrameworkBuilder()
     {
+        modeledOptions = Collections.singleton(ModeledOptions.ignoreMissingNodesForChildren);
     }
 
     ModeledFrameworkBuilder(AsyncCuratorFramework client, ModelSpec<T> modelSpec)
