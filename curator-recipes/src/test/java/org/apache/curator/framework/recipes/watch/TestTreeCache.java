2bbdbfd5f964ce2604c9a6a304f247da98361ab5
randgalt <randgalt@apache.org>
2017-07-31 09:43:18 -0500

    event order has changed a bit

--- curator-recipes/src/test/java/org/apache/curator/framework/recipes/watch/TestTreeCache.java
+++ curator-recipes/src/test/java/org/apache/curator/framework/recipes/watch/TestTreeCache.java
@@ -18,7 +18,7 @@
  */
 package org.apache.curator.framework.recipes.watch;
 
-import org.apache.curator.test.KillSession;
+import org.apache.curator.test.compatibility.KillSession2;
 import org.apache.curator.utils.CloseableUtils;
 import org.apache.zookeeper.CreateMode;
 import org.testng.Assert;
@@ -401,9 +401,9 @@ public class TestTreeCache extends BaseTestTreeCache
         client.create().withMode(CreateMode.EPHEMERAL).forPath("/test/me", "data".getBytes());
         assertEvent(CacheEvent.NODE_CREATED, "/test/me");
 
-        KillSession.kill(client.getZookeeperClient().getZooKeeper());
-        assertEvent(CacheEvent.NODE_DELETED, "/test/me", "data".getBytes());
+        KillSession2.kill(client.getZookeeperClient().getZooKeeper());
         assertEvent(CacheEvent.CACHE_REFRESHED);
+        assertEvent(CacheEvent.NODE_DELETED, "/test/me", "data".getBytes());
 
         assertNoMoreEvents();
     }
