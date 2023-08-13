d14cb464cbd4e8e0e1bb51daba76e84eb2e346db
Kezhu Wang <kezhuw@apache.org>
2023-08-13 23:39:17 +0800

    CURATOR-457: Custom service discovery path (#473)
    Signed-off-by: tison <wander4096@gmail.com>
    Co-authored-by: alexm <alexm25890@gmail.com>
    Co-authored-by: tison <wander4096@gmail.com>

---
+++ curator-x-discovery/src/main/java/org/apache/curator/x/discovery/details/DiscoveryPathConstructorImpl.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.curator.x.discovery.details;
+
+import com.google.common.base.Preconditions;
+import org.apache.curator.utils.PathUtils;
+import org.apache.curator.utils.ZKPaths;
+import org.apache.curator.x.discovery.DiscoveryPathConstructor;
+
+/**
+ * A standard constructor, it uses standard path constructing strategy by applying name to the base path.
+ */
+public class DiscoveryPathConstructorImpl implements DiscoveryPathConstructor {
+    private final String basePath;
+
+    public DiscoveryPathConstructorImpl(String basePath) {
+        Preconditions.checkArgument(basePath != null, "basePath cannot be null");
+        PathUtils.validatePath(basePath);
+        this.basePath = basePath;
+    }
+
+    @Override
+    public String getBasePath() {
+        return basePath;
+    }
+
+    @Override
+    public String getPathForInstances(String serviceName) {
+        return ZKPaths.makePath(basePath, serviceName);
+    }
+}
---
+++ curator-x-discovery/src/test/java/org/apache/curator/x/discovery/details/DiscoveryPathConstructorImplTest.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.curator.x.discovery.details;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.Test;
+
+public class DiscoveryPathConstructorImplTest {
+    @Test
+    public void testCanonicalDiscoveryPathConstructor() {
+        assertThatThrownBy(() -> new DiscoveryPathConstructorImpl(null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new DiscoveryPathConstructorImpl("")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new DiscoveryPathConstructorImpl("foo/bar"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new DiscoveryPathConstructorImpl("foo/bar/"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new DiscoveryPathConstructorImpl("/foo/bar/"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void testGetBasePath() {
+        assertThat(new DiscoveryPathConstructorImpl("/foo/bar").getBasePath()).isEqualTo("/foo/bar");
+        assertThat(new DiscoveryPathConstructorImpl("/").getBasePath()).isEqualTo("/");
+    }
+
+    @Test
+    public void testGetPathForInstances() {
+        DiscoveryPathConstructorImpl constructor = new DiscoveryPathConstructorImpl("/foo/bar");
+        assertThat(constructor.getPathForInstances("baz")).isEqualTo("/foo/bar/baz");
+        assertThat(constructor.getPathForInstances("")).isEqualTo("/foo/bar");
+        assertThat(constructor.getPathForInstances(null)).isEqualTo("/foo/bar");
+    }
+}
